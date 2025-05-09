ee5eb42914b9419b3e6640cd0804303aba943bb6
adarshtiwari789 <aadarshh1789@gmail.com>
2025-05-09 21:05:12 +0530

    Add routes ,seperate concern of mongoose , added owners model

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express()
 
 const cookieParser = require("cookie-parser")
 const path = require("path")
+const ownersRoutter = require("./routes/ownersRoutter")
+const usersRoutter = require("./routes/usersRoutter")
+const productsRoutter = require("./routes/productsRoutter")
+const db = require("./config/mongoose-connection")
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname , "public")))
 app.set('view engine', 'ejs');
+ 
+app.use("/owners", ownersRoutter);
+app.use("/users", usersRoutter);
+app.use("/products", productsRoutter);
 
-app.get("/" , (req,res)=>{
-    res.render("index")
+
+app.listen(3000,()=>{
+    console.log("server start")
 })
-
-app.listen(3000)
